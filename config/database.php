b798f206dd74af0ac181c152a47c506c7044833c
Jereu <jastudim@outlook.com>
2018-12-14 10:24:00 +0000

    add information to project

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,11 +58,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-247-102-1.eu-west-1.compute.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd2alh40hi33514'),
+            'username' => env('DB_USERNAME', 'tjvyojbrtrdjfo'),
+            'password' => env('DB_PASSWORD', 'b409bbb88754e987a9e6918a66dbca93d1d6fa979084a2710005a52ae2dede62'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
